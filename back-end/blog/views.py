765563671bf1002874bdf83bf03236e55206ca8c
reza <mardanireza58@gmail.com>
2024-03-20 19:39:12 +0330

    write model and view for Post

--- back-end/blog/views.py
+++ back-end/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import status
+
 
 # Create your views here.
+
+class PostView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            new_post = Post.objects.create(title=request.data['title'], description=request.data['description'])
+            new_post.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'message': 'error creating post!'}, status=status.HTTP_400_BAD_REQUEST)

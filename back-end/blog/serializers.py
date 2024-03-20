957c3d8fc3d7626ac6eb4fdaf0293059a84d637e
reza <mardanireza58@gmail.com>
2024-03-20 23:08:00 +0330

    add: user view and serializer

--- back-end/blog/serializers.py
+++ back-end/blog/serializers.py
@@ -1,6 +1,23 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class PostSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200)
     description = serializers.CharField(max_length=200)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'username')
+        extra_kwargs = {
+            'username': {'required': False}
+        }
+
+    def create(self, validated_data):
+        username = validated_data['email']
+        user = User.objects.create_user(username=username,**validated_data)
+        return user
 
--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}

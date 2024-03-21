9a041f6a33b2676ca7b64b114c8a63e3562fc476
reza <mardanireza58@gmail.com>
2024-03-21 20:45:45 +0330

    add: account module

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig'
+    # module
     'rest_framework',
     'rest_framework_simplejwt',
     "corsheaders"

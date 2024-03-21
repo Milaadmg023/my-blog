7d9b89a6414023d71fbad26ccf358ab5d70214a5
reza <mardanireza58@gmail.com>
2024-03-21 20:51:10 +0330

    config and update account module

--- back-end/account/views.py
+++ back-end/account/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "user": serializer.data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token)
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUserView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = User.objects.filter(email__iexact=email).first()
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                "access": str(refresh.access_token)
+            }, status=status.HTTP_200_OK)
+        return Response({'error': "user not found"}, status=status.HTTP_401_UNAUTHORIZED)
--- back-end/blog/views.py
+++ back-end/blog/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
-from .serializers import PostSerializer, UserSerializer
+from .serializers import PostSerializer
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-
 
 # Create your views here.
 
@@ -24,32 +20,3 @@ class PostView(APIView):
             new_post.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response({'message': 'error creating post!'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RegisterUserView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "user": serializer.data,
-                "refresh": str(refresh),
-                "access": str(refresh.access_token)
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LoginUser(APIView):
-    def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
-
-        user = User.objects.filter(email__iexact=email).first()
-        if user and user.check_password(password):
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                "access": str(refresh.access_token)
-            }, status=status.HTTP_200_OK)
-        return Response({'error': "user not found"}, status=status.HTTP_401_UNAUTHORIZED)

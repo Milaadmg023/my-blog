655b617a210c66be184c0f300e7b9632a58ec9b9
reza <mardanireza58@gmail.com>
2024-03-21 13:45:28 +0330

    add: cros header

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'django-insecure-zi)qzrau!25)nky!846ct%qoc2zeo@#4t9-bm2zn=*-phxpaon
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS  = True
 
 # Application definition
 
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware"
 ]
 
 ROOT_URLCONF = 'config.urls'

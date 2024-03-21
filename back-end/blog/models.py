04675b472e31669d47c8261bde5ec2342f0d46d7
reza <mardanireza58@gmail.com>
2024-03-21 21:13:58 +0330

    update

--- back-end/blog/models.py
+++ back-end/blog/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_jalali.db import models as jmodels
+
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = jmodels.jDateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

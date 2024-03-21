86f1bf4e494644c10d6a05d30afbe8defc5bdd8a
reza <mardanireza58@gmail.com>
2024-03-21 20:51:10 +0330

    config and update account module

--- back-end/blog/serializers.py
+++ back-end/blog/serializers.py
@@ -4,20 +4,3 @@ from django.contrib.auth.models import User
 class PostSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200)
     description = serializers.CharField(max_length=200)
-
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ('email', 'password', 'username')
-        extra_kwargs = {
-            'username': {'required': False}
-        }
-
-    def create(self, validated_data):
-        username = validated_data['email']
-        user = User.objects.create_user(username=username,**validated_data)
-        return user
-

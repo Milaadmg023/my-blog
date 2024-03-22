1911555b8538dbec3c9cb84f6cb7223356e52c85
reza <mardanireza58@gmail.com>
2024-03-22 09:24:48 +0330

    debug: send post

--- back-end/blog/serializers.py
+++ back-end/blog/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Category, Post
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=200)
-    description = serializers.CharField(max_length=200)
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'description', 'category', 'author')
+        extra_kwargs = {
+            "author": {'required': False}
+        }

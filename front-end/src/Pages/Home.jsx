35c7d758153cf340fe3c0bda7c5a3666147eeee1
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-03-31 08:33:11 +0330

    adding api

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -8,15 +8,12 @@ import Tags from '../Components/Tags';
 
 
 function Home() {
+  const [posts , setPosts] = React.useState([])
   const getPosts = async()=>{
     try {
-      await fetch("http://127.0.0.1:8000/blog/posts")
-      .then((res)=>{
-        return res.json()
-      })
-      .then((data)=>{
-        console.log(data);
-      })
+      const data = await fetch("http://127.0.0.1:8000/blog/posts")
+      const res = await data.json()
+      setPosts(res)
     } catch (error) {
       console.log(error);
     }
@@ -24,12 +21,15 @@ function Home() {
   React.useEffect(()=>{
     getPosts()
   }, [])
+
+  const lastPosts = posts.slice(0, 2)
+  console.log(lastPosts , posts);
   return (
-    <>
-    <NewPosts/>
+    <main className='mx-1'>
+    <NewPosts last={lastPosts}/>
     <div className='grid grid-cols-1 lg:grid-cols-3 lg:w-[80%] mx-auto mt-6'>
       <div className='col-span-2'>
-        <AllPosts />
+        <AllPosts posts={posts}/>
       </div>
       
       <div className='pt-2 lg:p-0'>
@@ -39,7 +39,7 @@ function Home() {
         <Tags/>
       </div>
     </div>
-    </>
+    </main>
   )
 }
 
